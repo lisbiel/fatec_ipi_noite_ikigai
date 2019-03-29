2b25ba40155560a7c3ecf4b0cffce03557c2d8d0
lisbiel <lisbiel@gmail.com>
2019-03-29 20:40:56 -0300

    Meio feito com Views e XML

---
+++ app/src/main/java/br/com/lisboa/fatec_ipi_noite_paint/DoodleView.java
@@ -0,0 +1,13 @@
+package br.com.lisboa.fatec_ipi_noite_paint;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DoodleView extends View {
+
+    public DoodleView(Context context, AttributeSet set) {
+        super(context, set);
+    }
+}
