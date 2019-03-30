6400fed96f501a2351c268114f7ea214f26d1bb0
lisbiel <lisbiel@gmail.com>
2019-03-29 22:15:15 -0300

    Views Prontas

---
+++ app/src/main/java/br/com/lisboa/fatec_ipi_noite_paint/EraseImageDialogFragment.java
@@ -0,0 +1,6 @@
+package br.com.lisboa.fatec_ipi_noite_paint;
+
+import android.support.v4.app.DialogFragment;
+
+public class EraseImageDialogFragment extends DialogFragment {
+}
---
+++ app/src/main/java/br/com/lisboa/fatec_ipi_noite_paint/LineWidthDialogFragment.java
@@ -0,0 +1,7 @@
+package br.com.lisboa.fatec_ipi_noite_paint;
+
+import android.support.v4.app.DialogFragment;
+
+public class LineWidthDialogFragment extends DialogFragment {
+
+}
